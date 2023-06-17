a0b1c9593a3550954d770657ee31f4997a8d534c
Xp4blos <pawelleszekorlik@gmail.com>
2023-06-17 14:12:51 +0200

    json
    okok

---
+++ json.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_GET['r']))
+{
+$req = $_GET['r'];
+
+if($req == 1)
+$conn = new mysqli("127.0.0.1:3306", "root", "", "otwock");
+$stmt = $conn->prepare("SELECT events.name, events.des, kategorie.nazwa FROM events INNER JOIN kategorie ON kategorie.id = events.kategoria_id WHERE events.status = 1");
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+}
+?>
