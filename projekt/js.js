f0db1f71b4b204f9b1da06e1d113a89d9f07cd83
L3ukocyt <igormrozek@outlook.com>
2023-06-17 18:19:26 +0200

    ulice
    ulice

--- projekt/js.js
+++ projekt/js.js
@@ -1,9 +1,8 @@
 var map = L.map('map', {
   center: [52.1023, 21.2669],
   zoom: 12,
-  doubleClickZoom: false
+  doubleClickZoom: false 
 });
-
 
 var p = document.getElementById('p');
 
@@ -18,17 +17,27 @@ map.on('dblclick', function(event) {
 
   var marker = L.marker(latLng).addTo(map);
   markers.push(marker);
+  //Wyświetlanie pina
+  fetchStreetNames(latLng)
+    .then(function(streetNames) {
+      var popupContent = '<p id="zdarzenie">woda</p>' + '<p id="idZdarzenia">1</p>';
+      if (streetNames.length > 0) {
+        popupContent += '<p>Ulice:</p><ul>';
+        streetNames.forEach(function(name) {
+          popupContent += '<li>' + name + '</li>';
+        });
+        popupContent += '</ul>';
+      }
+      marker.bindPopup(popupContent).openPopup();
 
-  // var marker2 = L.marker([52.1015,21.2998]).addTo(map);
-
-  var popupContent = '<p id="zdarzenie">woda</p>' + '<p id="idZdarzenia">1</p>' 
-  // + latLng.lat.toFixed(4) + '</br>' + latLng.lng.toFixed(4);
-  marker.bindPopup(popupContent).openPopup();
-
-  var deleteButton = marker.getPopup().getContent().querySelector('.deleteButton');
-  deleteButton.addEventListener('click', function() {
-    deleteMarker(marker);
-  });
+      var deleteButton = marker.getPopup().getContent().querySelector('.deleteButton');
+      deleteButton.addEventListener('click', function() {
+        deleteMarker(marker);
+      });
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
 
   console.log('Clicked coordinates:', latLng);
 });
@@ -50,37 +59,54 @@ deleteButton.addEventListener('click', function() {
 });
 
 var drawnItems = new L.FeatureGroup().addTo(map);
-    var drawControl = new L.Control.Draw({
-      edit: {
-        featureGroup: drawnItems
-      },
-      draw: {
-        polygon: true,
-        rectangle: true,
-        circle: true,
-        circlemarker: false,
-        polyline: false,
-        marker: false
+var drawControl = new L.Control.Draw({
+  edit: {
+    featureGroup: drawnItems
+  },
+  draw: {
+    polygon: true,
+    rectangle: true,
+    circle: true,
+    circlemarker: false,
+    polyline: false,
+    marker: false
+  }
+});
+map.addControl(drawControl);
+
+map.on(L.Draw.Event.CREATED, function(event) {
+  var layer = event.layer;
+  drawnItems.addLayer(layer);
+});
+
+map.on(L.Draw.Event.EDITED, function(event) {
+  var layers = event.layers;
+  layers.eachLayer(function(layer) {
+    // Warstwa
+  });
+});
+
+map.on(L.Draw.Event.DELETED, function(event) {
+  var layers = event.layers;
+  layers.eachLayer(function(layer) {
+    // Warstwa
+  });
+});
+
+//Dodawanie ulic
+function fetchStreetNames(latLng) {
+  var url = 'https://nominatim.openstreetmap.org/reverse?format=json&lat=' + latLng.lat + '&lon=' + latLng.lng;
+  return fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var streetNames = [];
+      if (data.address && data.address.road) {
+        streetNames.push(data.address.road);
       }
+      
+      return streetNames;
     });
-    map.addControl(drawControl);
-
-    map.on(L.Draw.Event.CREATED, function(event) {
-      var layer = event.layer;
-      drawnItems.addLayer(layer);
-    });
-
-    map.on(L.Draw.Event.EDITED, function(event) {
-      var layers = event.layers;
-      layers.eachLayer(function(layer) {
-        // tutaj warstwa nowa
-      });
-    });
-
-    map.on(L.Draw.Event.DELETED, function(event) {
-      var layers = event.layers;
-      layers.eachLayer(function(layer) {
-        // Tutaj warstwa
-      });
-    });
+}
 
